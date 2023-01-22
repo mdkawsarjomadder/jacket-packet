9c8feed1e6bf56d2d101bfcc51d92372ada5ff6c
mdkawsar <mdkawsar342814@gamil.com>
2023-01-23 00:50:34 +0600

    kawsar

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        price: '#FABE4C',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+  },
+  plugins: [],
+};
